66dc4c62359f9b1622ecb17b5ca75ac83550bff0
root <zhopen>
2018-11-16 15:35:28 +0800

    add a nodeosd3 to install

---
+++ test.sh
@@ -0,0 +1,10 @@
+ 
+#Your can do some test using below  
+test contract "hello"
+cleos set contract hello ../contracts/hello  -p hello@active
+cleos push action hello hi '["bob"]' -p bob@active
+
+#switch producer from eosio to inita
+cleos push action eosio setprods '{ "schedule": [{"producer_name": "inita","block_signing_key": "EOS6hMjoWRF2L8x9YpeqtUEcsDKAyxSuM1APicxgRU1E3oyV5sDEg"}]}' -p eosio@active
+cleos push action eosio setprods '{ "schedule": [{"producer_name": "initb","block_signing_key": "EOS6hMjoWRF2L8x9YpeqtUEcsDKAyxSuM1APicxgRU1E3oyV5sDEg"}]}' -p eosio@active
+
