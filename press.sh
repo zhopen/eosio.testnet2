d4d4785384209d5c27d16f16a49ef3fb20f637d6
root <zhopen>
2018-12-06 11:15:24 +0800

    fixed

--- press.sh
+++ press.sh
@@ -4,27 +4,31 @@
 
 
 #export cleos='docker exec -it keosd /opt/eosio/bin/cleos --url http://172.30.0.101:8888 --wallet-url http://172.30.0.100:8888 '
-export cleos='cleos --url http://127.0.0.1:8888 --wallet-url http://172.30.0.100:8888 '
+export cleos='cleos --url http://172.30.101:8888 --wallet-url http://172.30.0.100:8888 '
 START_SECOND=`date +%s`
 echo “start pressing”
-start_balance=`$cleos get currency balance  eosio.token alice eos | awk -F. '{print $1}'`
+START_BALANCE=`$cleos get currency balance  eosio.token alice eos | awk -F. '{print $1}'`
 
-for((i=1;i<=$1;i++))
+
+ACCOUNTS_FILE=./accounts.txt
+LOG_FILE=./press.log
+
+for((i=0;i<$1;i++));
 do
-xargs -n 1 -I '#' -P 200  $cleos push action eosio.token transfer '[ "alice", "#", "1.0000 EOS", "" ]' -p alice@active <accounts.txt  >press.log 2>&1
+xargs -a $ACCOUNTS_FILE -n 1 -I '#' -P 200  $cleos push action eosio.token transfer '[ "alice", "#", "1.0000 EOS", "" ]' -p alice@active  >$LOG_FILE 2>&1;
 done
 
 sleep 1s
-end_balance=`$cleos get currency balance  eosio.token alice eos | awk -F. '{print $1}'`
-transfer_count=`expr $end_balance - $start_balance`
-echo 'transfer count:' $transfer_count
+END_BALANCE=`$cleos get currency balance  eosio.token alice eos | awk -F. '{print $1}'`
+TRANSFER_TOTAL=`expr $END_BALANCE - $START_BALANCE`
+echo 'transfer count:' $TRANSFER_TOTAL
 
 END_SECOND=`date +%s`
-duration=`expr $END_SECOND - $START_SECOND`
-echo "运行时长:"  $duration
+DURATION=`expr $END_SECOND - $START_SECOND`
+echo "运行时长:"  $DURATION
 
-account_total=`wc -l < accounts.txt`
-trx_total=`expr $account_total \* $1`
-echo "trx tps:" `expr  $trx_total / $duration`
+ACCOUNT_TOTAL=`wc -l < accounts.txt`
+TRANSACTION_TOTAL=`expr $ACCOUNT_TOTAL \* $1`
+echo "trx requested:" `expr  $TRANSACTION_TOTAL / $DURATION`
 
-echo "successful trx tps:" `expr $transfer_count / $duration` 
+echo "successful trx tps:" `expr $TRANSFER_TOTAL / $DURATION` 
